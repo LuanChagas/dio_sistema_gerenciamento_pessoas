2f2d4116a71e5237c6d09e45cc056eea45a60e06
Luan Chagas <luanchagas@hotmail.com>
2021-09-04 18:44:13 -0300

    developed update by id feature

--- src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,7 +29,6 @@ public class PersonController {
     PersonService personService;
 
     @PostMapping
-
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
@@ -43,6 +43,11 @@ public class PersonController {
     @GetMapping("/{id}")
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
+    }
+
+    @PutMapping
+    public MessageResponseDTO updateById(@RequestBody PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(personDTO);
     }
 
     @DeleteMapping("/{id}")
